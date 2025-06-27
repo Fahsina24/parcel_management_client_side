efc9c2560836de750d7e9e13a6e5b4cb088d5a94
Fahsina24 <fahsinahossain10@gmail.com>
2025-06-27 23:14:52 +0100

    My Parcels page update, cancel button and their fuctinalities are completed.

--- src/pages/Dashboard/User/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/User/MyParcels/MyParcels.jsx
@@ -1,11 +1,42 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../../../provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useContext(AuthContext);
   const myParcels = useLoaderData();
-  // console.log(myParcels);
+  const [listedData, setListedData] = useState(myParcels);
+  const handleCancel = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Cancel it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/cancel/${id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            const newListedData = listedData.map((data) =>
+              id === data._id ? { ...data, status: "cancelled" } : data
+            );
+            setListedData(newListedData);
+            Swal.fire({
+              title: "Cancelled!",
+              text: "Your parcel booking status has been updated.",
+              icon: "success",
+            });
+          });
+      }
+    });
+    // console.log(myParcels);
+  };
   return (
     <div className="overflow-x-auto rounded-lg bg-base-100 border-gray-50 border-2">
       <table className="table ">
@@ -26,7 +57,7 @@ const MyParcels = () => {
           </tr>
         </thead>
         <tbody>
-          {myParcels.map((parcel, index) => (
+          {listedData.map((parcel, index) => (
             <tr key={index}>
               <th>{index + 1}</th>
               <td>{parcel.parcelType}</td>
@@ -45,10 +76,21 @@ const MyParcels = () => {
                 )}
               </td>
               <td>
-                <button className="btn">Cancel</button>
+                {parcel.status == "pending" ? (
+                  <button
+                    className="btn"
+                    onClick={() => handleCancel(parcel._id)}
+                  >
+                    Cancel
+                  </button>
+                ) : (
+                  <button className="btn disabled bg-gray-200">Cancel</button>
+                )}
               </td>
               <td>
-                <button className="btn ">Review</button>
+                {parcel.status == "delivered" && (
+                  <button className="btn ">Review</button>
+                )}
               </td>
               <td>
                 <button className="btn ">Pay</button>
